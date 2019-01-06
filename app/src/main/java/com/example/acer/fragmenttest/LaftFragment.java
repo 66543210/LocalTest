a4231166bf8972f2aed6764c6b29645d18eb7e35
66543210 <1078419568@qq.com>
2019-01-06 10:55:16 +0800

    First commit

---
+++ app/src/main/java/com/example/acer/fragmenttest/LaftFragment.java
@@ -0,0 +1,21 @@
+package com.example.acer.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Acer on 2019/1/4.
+ */
+
+public class LaftFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/acer/fragmenttest/RightFragment.java
@@ -0,0 +1,20 @@
+package com.example.acer.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Acer on 2019/1/4.
+ */
+
+public class RightFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.right_fragment,container,false);
+    }
+}
